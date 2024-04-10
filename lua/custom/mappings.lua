4c3029c989f71c0b0121b2a07e83d15000778d37
Isaac Wassouf <isaacwassouf@gmail.com>
2024-04-10 14:54:50 +0200

    feat: move line(s) up and down in normal and visual mode

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.general = {
+  n = {
+    ["<A-j>"] = {":m .+1<CR>==", "Move line up(n)"},
+    ["<A-k>"] = {":m .-2<CR>==", "Move line down(n)"}
+  },
+
+  v = {
+    ["<A-j>"] = {":m '>+1<CR>gv=gv", "Move line up(v)"},
+    ["<A-k>"] = {":m '<-2<CR>gv=gv" ,"Move line down(v)"}
+  }
+}
+
+return M
